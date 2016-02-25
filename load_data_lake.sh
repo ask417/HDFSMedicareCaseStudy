bdf402525ff9a811cc2fad3be0322b7b75c03670
root <root@ip-172-31-11-53.ec2.internal>
2016-02-25 18:58:37 +0000

    Had to fix some scripts

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -1,13 +1,27 @@
-for file in /home/w205/hospital_compare/*.csv
+medicare= "Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip; charset=binary&filename=Hospital_Revised_Flatfiles.zip"
+
+if [-e  "$medicare"]
+then
+	echo "Medicare zip found"
+else
+	wget "https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
+fi
+
+medicareZip="Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip; charset=binary&filename=Hospital_Revised_Flatfiles.zip"
+
+unzip "$medicareZip" -d "/home/w205/HDFSMedicareCaseStudy/data/"
+
+for file in /home/w205/HDFSMedicareCaseStudy/data/*.csv 
 do
         #Renames and removes whitespaces from file name
-        new=new$(basename "$file")
-        new=${n//[[:blank:]]/}
-	new="/home/w205/hospital_compare/$new"
-        #Remove headers from all csv files
+        new=$(basename "$file")
+        new=${new//[[:blank:]]/}
+	new="/home/w205/HDFSMedicareCaseStudy/modified_csvs/$new"
+
+	#Remove headers from all csv files
         tail -n +2 "$file" > "$new"
 	rm -f "$file"
 
         #Make hdfs directory, and put file into it with hdfs dfs -put filename pathToJustAddedDirectory
-        hdfs dfs -put $(basename "$new") /user/w205/hospital_compare  
+        hdfs dfs -put "$new" /user/w205/hospital_compare  
 done
